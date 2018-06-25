8ae2480b29daa564229804698d2f4cf334faacb2
madhavparekh <madhavparekh@gmail.com>
2018-06-25 00:26:35 -0700

    updated fields

--- models/DailyData.js
+++ models/DailyData.js
@@ -8,37 +8,21 @@ var Schema = mongoose.Schema;
 var DailyDataSchema = new Schema({
 	date: {
 		type: Date,
-		get: getRecordDate,
 		required: true,
 	},
 
-	discharge_cfs: {
-		type: Number,
-		get: getCFS,
-		set: setCFS,
+	discharge: {
+		type: mongoose.SchemaTypes.Decimal128,
 		required: true,
 	},
 
 	//data belongs to gauges
-	gauge: {
-		type: Schema.Types.ObjectId,
+	gauge_id: {
+		type: Schema.Types.Number,
 		ref: 'Gauges',
 	},
 });
 
-//get discharge flow in decimal(2)
-function getCFS(num) {
-	return (num / 100).toFixed(2);
-}
-//set discharge flow from decimal to whole number
-function setCFS(num) {
-	return num * 100;
-}
-
-//get Date in YYYY-MM-DD format
-function getRecordDate(date) {
-	return date.toISOString().split('T')[0];
-}
 
 // This creates our model from the above schema, using mongoose's model method
 var DailyData = mongoose.model('DailyData', DailyDataSchema);
