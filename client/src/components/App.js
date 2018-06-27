bf200d5d94747780371342ce22fe755628aaf4a5
Ryan <rjlafazan@gmail.com>
2018-06-26 19:56:55 -0700

    basic components built without styling

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,19 +2,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Switch, Route } from "react-router-dom";
-import { MuiThemeProvider } from "@material-ui/core/styles";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
-import Theme from "../styles/Theme";
+// import Theme from "../styles/Theme";
 import Home from "../containers/Home";
 import Count from "../containers/Count";
 
 const App = () => (
-  <MuiThemeProvider theme={Theme}>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/count" component={Count} />
-    </Switch>
-  </MuiThemeProvider>
+  <div>
+    <MuiThemeProvider>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/count" component={Count} />
+      </Switch>
+    </MuiThemeProvider>
+  </div>
 );
 
 App.propTypes = {
---
+++ client/src/components/home/ViewCard.js
@@ -0,0 +1,51 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+
+import CardContent from "@material-ui/core/CardContent";
+
+import Typography from "@material-ui/core/Typography";
+import ViewBar from "./ViewBar";
+
+const styles = {
+  card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    marginBottom: 16,
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+};
+
+function ViewCard(props) {
+  const { classes } = props;
+  //   const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <div>
+      <Card className={classes.card}>
+        <CardContent>
+          <Typography className={classes.title} color="textSecondary">
+            Graph View Options
+          </Typography>
+          <ViewBar />
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+ViewCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ViewCard);
