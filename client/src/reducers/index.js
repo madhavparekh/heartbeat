c11d96ef00270fa21ca9f420e41ffbb17b32d2e8
Jackson Harris III <Apollo@Apollo.attlocal.net>
2018-06-29 07:14:04 -0700

    init guage reducer, update reducer index

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -4,10 +4,16 @@ import { routerReducer } from "react-router-redux";
 import todo from "./todo";
 import count from "./count";
 
+// water project
+import guage from "./guage"
+
 const rootReducer = combineReducers({
   routing: routerReducer,
   todo,
   count,
+  
+  //water project
+  guage,
 });
 
 export default rootReducer;
