f21c11d7970d1740d6e107afd5491883e3fa886b
Jackson Harris III <Apollo@Apollo.attlocal.net>
2018-06-29 07:10:29 -0700

    update actions & types for guages

--- client/src/action-types/index.js
+++ client/src/action-types/index.js
@@ -1,10 +1,19 @@
 import TodoTypes from "./todo";
 import CountTypes from "./count";
 
+//water project
+import GuageTypes from "./guage"
+
 export { TodoTypes };
 export { CountTypes };
+
+//water project
+export { GuageTypes };
 
 export default {
   TodoTypes,
   CountTypes,
+  
+  //water project
+  GuageTypes,
 };
