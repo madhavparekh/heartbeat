e585530c40d43a8340c6318f48ac70afc61f5093
madhavparekh <madhavparekh@gmail.com>
2018-06-29 21:54:41 -0700

    added api routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var {
+	impaired,
+	unImpaired,
+	impairedAggregate,
+	unImpairedAggreate,
+	users,
+} = require('../controllers');
 
-/* GET home page. */
-router.get('/api/impaired/gauge/:id', function(req, res) {
-  res.render('index', { title: 'Express' });
+/* GET api impaired data from gauge_id*/
+router.get('/api/impaired/gauge/:gauge_id', (req, res) => {
+	console.log(req.params.gauge_id);
+	impaired.show(req, res);
+});
+
+/* GET api unimpaired data from gauge_id*/
+router.get('/api/unimpaired/gauge/:gauge_id', (req, res) => {
+	unImpaired.show(req, res);
+});
+
+/* GET api impairedAggregate data from gauge_id*/
+router.get('/api/impairedaggregate/gauge/:gauge_id', (req, res) => {
+	impairedAggregate.show(req, res);
+});
+/* GET api impaired data from gauge_id*/
+router.get('/api/unimpairedaggregate/gauge/:gauge_id', (req, res) => {
+	unImpairedAggreate.show(req, res);
 });
 
 module.exports = router;
