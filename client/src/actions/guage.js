d5a65a6d087d562007e65b7043447a29ee6b3492
madhavparekh <madhavparekh@gmail.com>
2018-07-03 21:15:00 -0700

    merging

--- client/src/actions/guage.js
+++ client/src/actions/guage.js
@@ -2,7 +2,6 @@ import request from 'superagent';
 
 import { GuageTypes as types } from '../action-types';
 
-<<<<<<< HEAD
 const fetchGuageObjects = guages => {
     console.log(guages)
     return {
@@ -25,23 +24,3 @@ export function fetchGuages() {
         }
     };
 }
-=======
-const fetchGuageObjects = (guages) => {
-  return {
-    type: types.FETCH_GUAGE_OBJECTS,
-    guages,
-  };
-};
-
-export function fetchGuages() {
-  return async (dispatch) => {
-    try {
-      const guagesFromAPI = await request.get('/api/gauges');
-      // pretent this is an api call...
-      dispatch(fetchGuageObjects(guagesFromAPI.body));
-    } catch (e) {
-      throw e;
-    }
-  };
-}
->>>>>>> redux for flow data
