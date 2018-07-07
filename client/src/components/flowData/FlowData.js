958aa2ed4887568d2b1df711308878d3d9ef4559
Jackson Harris III <Apollo@Apollo.attlocal.net>
2018-07-07 00:12:30 -0700

    rework redux for flowData in preparation for display to graph

--- client/src/components/flowData/FlowData.js
+++ client/src/components/flowData/FlowData.js
@@ -8,28 +8,28 @@ class FlowData extends Component  {
   render(){
     return (
       <div>
-          <Button onClick={() => props.fetchImpairedData(props.gauge_id)}>
+          <Button onClick={() => this.props.fetchImpairedData(this.props.gauge_id)}>
             Get Impaired Data
           </Button>
-          <Button onClick={() => props.fetchUnImpairedData(props.gauge_id)}>
+          <Button onClick={() => this.props.fetchUnImpairedData(this.props.gauge_id)}>
             Get UnImpaired Data
           </Button>
-          <Button onClick={() => props.fetchImpairedAggrData(props.gauge_id)}>
+          <Button onClick={() => this.props.fetchImpairedAggrData(this.props.gauge_id)}>
             Get Impaired Aggr Data
           </Button>
-          <Button onClick={() => props.fetchUnImpairedAggrData(props.gauge_id)}>
+          <Button onClick={() => this.props.fetchUnImpairedAggrData(this.props.gauge_id)}>
             Get UnImpaired Aggr Data
           </Button>
           <ul>
-            {props.impairedAggr && (
-              <p>Length of impaired aggr: {props.impairedAggr.length}</p>
+            {this.props.impairedAggr && (
+              <p>Length of impaired aggr: {this.props.impairedAggr.length}</p>
             )}
-            {props.unImpairedAggr && (
-              <p>Length of un impaired aggr: {props.unImpairedAggr.length}</p>
+            {this.props.unImpairedAggr && (
+              <p>Length of un impaired aggr: {this.props.unImpairedAggr.length}</p>
             )}
-            {props.impaired && <p>Length of impaired: {props.impaired.length}</p>}
-            {props.unImpaired && (
-              <p>Length of un impaired: {props.unImpaired.length}</p>
+            {this.props.impaired && <p>Length of impaired: {this.props.impaired.length}</p>}
+            {this.props.unImpaired && (
+              <p>Length of un impaired: {this.props.unImpaired.length}</p>
             )}
           </ul>
         </div>)
--- client/src/components/flowData/Layout.js
+++ client/src/components/flowData/Layout.js
@@ -1,15 +1,47 @@
 import React from "react";
 import PropTypes from "prop-types";//eslint-disable-line
 import FlowData from "./FlowData";
+import { fetchImpairedAggrData } from "../../actions/flowData";
 
-const Layout = () => (
-    <FlowData
-    />
+type Props = {
+    gauge_id: string,
+    fetchImpairedData: Function,
+    fetchUnImpairedData: Function,
+    fetchUnImpairedAggrData: Function,
+    fetchImpairedAggrData: Function,
+    unImpairedAggr: Array,
+    unImpaired: Array,
+    impaired: Array,
+    impairedAggr: Array
+
+}
+
+const Layout = (props: Props) => (
+    <div>
+        <FlowData
+            gauge_id={props.gauge_id}
+            fetchImpairedData={props.fetchImpairedData}
+            fetchUnImpairedData={props.fetchUnImpairedData}
+            fetchUnImpairedAggrData={props.fetchUnImpairedAggrData}
+            fetchImpairedAggrData={props.fetchImpairedAggrData}
+            unImpairedAggr={props.unImpairedAggr}
+            impaired={props.impaired}
+            impairedAggr={props.impairedAggr}
+            
+        />
+    </div>    
 );
 
 Layout.propTypes = {
-    // todos: PropTypes.array.isRequired,
-    // fetchTodos: PropTypes.func.isRequired,
+    gauge_id: PropTypes.string.isRequired,
+    fetchImpairedData: PropTypes.func.isRequired,
+    fetchUnImpairedData: PropTypes.func.isRequired,
+    fetchUnImpairedAggrData: PropTypes.func.isRequired,
+    fetchImpairedAggrData: PropTypes.func.isRequired,
+    unImpairedAggr: PropTypes.array,
+    unImpaired: PropTypes.array,
+    impaired: PropTypes.array,
+    impairedAggr: PropTypes.array,
 };
 
 export default Layout;
