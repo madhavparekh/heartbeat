7767ec1916146afdf0aed69edc6e96781f22df1c
Jackson Harris III <Apollo@Apollo.localdomain>
2018-07-07 11:35:09 -0700

    graph works

--- client/src/components/staticGraph/Layout.js
+++ client/src/components/staticGraph/Layout.js
@@ -1,92 +1,13 @@
-import React, { Component} from "react";
-import * as d3 from 'd3';
+import React from "react";
+import PropTypes from 'prop-types';
+import StaticGraph from "./StaticGraph";
 
-const mockData = [
-  {
-    discharge: 2,
-    date: 1,
-  },
-  {
-    discharge: 4,
-      date: 2,
-  },
-  {
-    discharge: 6,
-      date: 3,
-  },
-  {
-    discharge: 8,
-      date: 4,
-  },
-  {
-    discharge: 10,
-      date: 5,
-  },
-  {
-    discharge: 12,
-      date: 6,
-  },
-  {
-    discharge: 14,
-      date: 7,
-  },
-    {
-        discharge: 16,
-        date: 8,
-    },
-    {
-        discharge: 18,
-        date: 9,
-    },
-    {
-        discharge: 22,
-        date: 10,
-    }
-];
-
-class StaticGraph extends Component {
-
-constructor() {
-    super();
-
-    this.xScale = d3.scaleLinear();
-    this.yScale = d3.scaleLinear();
-    this.line = d3.line()
-
+const Layout = ({data}) => {
+    return <StaticGraph data={data} /> 
 }
 
-componentDidMount() {
-    this.updateD3()
-}
+Layout.propTypes = {
+    data: PropTypes.array,
+};
 
-updateD3() {
-
-    const svg = d3.select(this.node).append("path")
-    const parseTime = d3.timeParse("%Y-%m-%d")
-    
-    this.xScale
-        .domain([0,10])
-        .range([0, 485]);
-
-    this.yScale
-        .domain([0,20])
-        .range([0, 485]);
-
-    this.line.x(d => this.xScale(d.date)).y(d => this.yScale(d.discharge))  
-
-    // console.log(d3.select(this.node))
-    svg.attr("d", this.line(mockData)).attr('stroke', 'black')
-}
-
-
-render() {
-    return (
-        <svg width="500" height="500" ref={(node) => (this.node = node)} />
-    )
-
-}
-
-
-}
-
-export default StaticGraph
+export default Layout
--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -2,44 +2,62 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-//water project
-import { fetchGuages } from '../actions/guage';
-// import { fetchDefaultData } from "../actions/record"
-
+import * as flow from '../actions/flowData';
 import Layout from '../components/home/Layout';
 
-export class Home extends React.Component {
+class Home extends React.Component {
+
+  componentDidMount(){
+    const gaugeId = '08251500';
+
+    this.props.fetchImpairedData(gaugeId);
+    this.props.fetchUnImpairedData(gaugeId)
+    this.props.fetchImpairedAggrData(gaugeId)
+    this.props.fetchUnImpairedAggrData(gaugeId);
+  
+  }
+  
   render() {
+
     return (
       <Layout
-      //water project
-      // fetchGuages={() => this.props.fetchGuages()}
-      // fetchDefaultData={() => this.props.fetchDefaultData()}
+        gaugeId='08251500'
+        unImpairedAggr={this.props.unImpairedAggr}
+        impairedAggr={this.props.impairedAggr}
+        impaired={this.props.impaired}
+        unImpaired={this.props.unImpaired}
       />
     );
   }
 }
 
-Home.propTypes = {
-  //water project
-  fetchGuages: PropTypes.func.isRequired,
-  // fetchDefaultData: PropTypes.func.isRequired
-};
-
 const mapStateToProps = (state) => {
   return {
-    //water project
-    guages: state.guage.guages,
-    // defaultView: state.record.data
+    impaired: state.flowData.impaired,
+    unImpaired: state.flowData.unImpaired,
+    impairedAggr: state.flowData.impairedAggr,
+    unImpairedAggr: state.flowData.unImpairedAggr,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    //water project
-    fetchGuages: () => dispatch(fetchGuages()),
-    // fetchDefaultData: () => dispatch(fetchDefaultData()),
+    fetchImpairedData: (gaugeId) => dispatch(flow.fetchImpairedData(gaugeId)),
+    fetchUnImpairedData: (gaugeId) => dispatch(flow.fetchUnImpairedData(gaugeId)),
+    fetchImpairedAggrData: (gaugeId) => dispatch(flow.fetchImpairedAggrData(gaugeId)),
+    fetchUnImpairedAggrData: (gaugeId) => dispatch(flow.fetchUnImpairedAggrData(gaugeId)),
   };
+};
+
+Home.propTypes = {
+  fetchUnImpairedData: PropTypes.func.isRequired,
+  fetchImpairedData: PropTypes.func.isRequired,
+  fetchImpairedAggrData: PropTypes.func.isRequired,
+  fetchUnImpairedAggrData: PropTypes.func.isRequired,
+  unImpairedAggr: PropTypes.array,
+  unImpaired: PropTypes.array,
+  impairedAggr: PropTypes.array,
+  impaired: PropTypes.array,
 };
 
 export default connect(
