ca1bc44d3a82ae3d67ee5b1c6bce389cb9f35bce
Jackson Harris III <Apollo@Apollo.attlocal.net>
2018-07-06 22:41:12 -0700

    path for graph card with dummy data

--- client/src/components/staticGraph/Layout.js
+++ client/src/components/staticGraph/Layout.js
@@ -4,52 +4,52 @@ import * as d3 from 'd3';
 const mockData = [
   {
     discharge: 2,
-    date: "1999-07-03",
+    date: 1,
   },
   {
     discharge: 4,
-      date: "1999-07-04",
+      date: 2,
   },
   {
     discharge: 6,
-      date: "1999-07-05",
+      date: 3,
   },
   {
     discharge: 8,
-      date: "1999-07-06",
+      date: 4,
   },
   {
     discharge: 10,
-      date: "1999-07-07",
+      date: 5,
   },
   {
     discharge: 12,
-      date: "1999-07-08",
+      date: 6,
   },
   {
     discharge: 14,
-      date: "1999-07-09",
+      date: 7,
   },
     {
         discharge: 16,
-        date: "1999-07-10",
+        date: 8,
     },
     {
         discharge: 18,
-        date: "1999-07-11",
+        date: 9,
     },
     {
         discharge: 22,
-        date: "1999-07-12",
+        date: 10,
     }
 ];
 
 class StaticGraph extends Component {
 
-constructor(props) {
+constructor() {
     super();
 
-    this.xScale = d3.scaleTime();
+    this.xScale = d3.scaleLinear();
     this.yScale = d3.scaleLinear();
     this.line = d3.line()
 
@@ -65,16 +65,17 @@ updateD3() {
     const parseTime = d3.timeParse("%Y-%m-%d")
     
     this.xScale
+        .domain([0,10])
         .range([0, 485]);
 
     this.yScale
         .domain([0,20])
         .range([0, 485]);
 
-    this.line.x(d => this.xScale( parseTime(d.date) )).y(d => this.yScale(d.discharge))  
+    this.line.x(d => this.xScale(d.date)).y(d => this.yScale(d.discharge))  
 
     // console.log(d3.select(this.node))
-    svg.attr("d", this.line(mockData))
+    svg.attr("d", this.line(mockData)).attr('stroke', 'black')
 }
 
 
