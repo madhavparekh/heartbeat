215b07738d20096732f6e44a1780c5ef47c85877
Leo Q <leoq91@gmail.com>
2018-07-06 20:19:34 -0700

    pm2

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const CronJob = require('cron').CronJob; //eslint-disable-line
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose/lib');
@@ -117,7 +116,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use(passport.initialize());
 // Bring in defined Passport Strategy
