a430dccae1049bd96d8033c6419d2cfa23d447dd
Jackson Harris III <Apollo@Apollo.localdomain>
2018-07-07 10:03:36 -0700

    prep for rebase

--- client/src/components/flowData/FlowData.js
+++ client/src/components/flowData/FlowData.js
@@ -2,10 +2,56 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from '@material-ui/core';
+import * as d3 from 'd3';
 
 class FlowData extends Component  {
+
+  constructor() {
+    super();
+
+    this.xScale = d3.scaleTime();
+    this.yScale = d3.scaleLinear();
+    this.line = d3.line()
+
+    
+    this.state = {
+      realData : null
+    }  
+    
+
+  }
+
+  componentDidMount() {
+    this.updateD3()
+  }
+
+  updateD3() {
+
+    const svg = d3.select(this.node).append("path")
+    const parseTime = d3.timeParse("%Y-%m-%d")
+
+    this.xScale
+      .range([0, 485]);
+
+    this.yScale
+      .domain([0, 20])
+      .range([0, 485]);
+
+    this.line.x(d => this.xScale(parseTime(d.date))).y(d => this.yScale(d.discharge))
+
+    // console.log(d3.select(this.node))
+    svg.attr("d", this.line(this.state.realData))
+  }
   
   render(){
+
+    if (this.realData != null) {
+      
+      return (
+        <svg width="500" height="500" ref={(node) => (this.node = node)} />
+      )
+    }
+
     return (
       <div>
           <Button onClick={() => this.props.fetchImpairedData(this.props.gauge_id)}>
@@ -32,16 +78,18 @@ class FlowData extends Component  {
               <p>Length of un impaired: {this.props.unImpaired.length}</p>
             )}
           </ul>
+
+      
         </div>)
       }  
   }
 
 FlowData.propTypes = {
-  gauge_id: PropTypes.string.isRequired,
-  fetchImpairedData: PropTypes.func.isRequired,
-  fetchUnImpairedData: PropTypes.func.isRequired,
-  fetchUnImpairedAggrData: PropTypes.func.isRequired,
-  fetchImpairedAggrData: PropTypes.func.isRequired,
+  gauge_id: PropTypes.string,
+  fetchImpairedData: PropTypes.func,
+  fetchUnImpairedData: PropTypes.func,
+  fetchUnImpairedAggrData: PropTypes.func,
+  fetchImpairedAggrData: PropTypes.func,
   unImpairedAggr: PropTypes.array,
   unImpaired: PropTypes.array,
   impaired: PropTypes.array,
