0e72d0e09f58ebcffd9f37e8bd2aa40ba2a1e8c2
Leo Q <leoq91@gmail.com>
2018-07-12 16:04:24 -0700

    add maplayer

--- client/src/components/map/BaseMap.js
+++ client/src/components/map/BaseMap.js
@@ -4,13 +4,16 @@ import PropTypes from "prop-types";
 import { fromJS } from "immutable";
 import { assign } from "lodash";
 
-import { defaultMapStyle } from "./map-style.js";
+import { defaultMapStyle, gaugeLayer } from "./map-style.js";
 
 class BaseMap extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       mapStyle: defaultMapStyle,
+      hoveredFeature: null,
+      x: null,
+      y: null,
       viewport: {
         height: 400,
         latitude: 36.2055,
@@ -25,6 +28,16 @@ class BaseMap extends React.Component {
     if (props.gauges.length < 1) {
       return null;
     }
+
+    // the following method will take the incoming gauges, and append them onto map layers
+
+    const newCombinedLayer = fromJS(
+      defaultMapStyle
+        .get("layers")
+        .toJS()
+        .concat(gaugeLayer)
+    );
+
     const combinedGauges = {
       gauges: {
         data: { type: "FeatureCollection", features: [] },
@@ -43,7 +56,7 @@ class BaseMap extends React.Component {
         type: "Feature",
         geometry: {
           type: "Point",
-          coordinates: [gauge.latitude, gauge.longitude],
+          coordinates: [gauge.longitude, gauge.latitude],
         },
       };
       combinedGauges.gauges.data.features.push(
@@ -51,23 +64,61 @@ class BaseMap extends React.Component {
       );
     });
 
-    const mapStyle = defaultMapStyle.set(
-      "sources",
-      fromJS(assign({}, defaultMapStyle.get("sources").toJS(), combinedGauges))
-    );
+    const mapStyle = defaultMapStyle
+      .set(
+        "sources",
+        fromJS(
+          assign({}, defaultMapStyle.get("sources").toJS(), combinedGauges)
+        )
+      )
+      .set("layers", newCombinedLayer);
 
     return { mapStyle };
   }
 
+  _onHover(event) {
+    const {
+      features,
+      srcEvent: { offsetX, offsetY },
+    } = event;
+    if (features.find(f => f.layer.id.indexOf("gauges") >= 0)) {
+      const hoveredFeature =
+        features && features.find(f => f.layer.id.indexOf("gauge") >= 0);
+      return this.setState({ hoveredFeature, x: offsetX, y: offsetY });
+    } else {
+      return this.setState({ hoveredFeature: null, x: null, y: null });
+    }
+  }
+
+  _renderTooltip() {
+    const { hoveredFeature, x, y } = this.state;
+    if (!hoveredFeature || !x || !y) {
+      return null;
+    }
+
+    return (
+      <div
+        className="tooltip"
+        style={{ position: "absolute", left: x, top: y }}
+      >
+        <div>{hoveredFeature.properties.description}</div>
+      </div>
+    );
+  }
+
   render() {
     return (
-      <div style={{ margin: "20px auto" }}>
+      <div style={{ margin: "20px auto", position: "relative" }}>
         <ReactMapGL
+          maxZoom={8}
+          minZoom={5}
+          onHover={e => this._onHover(e)}
           mapStyle={this.state.mapStyle}
           mapboxApiAccessToken="pk.eyJ1IjoibGVvZ29lc2dlciIsImEiOiJjamU3dDEwZDkwNmJ5MnhwaHM1MjlydG8xIn0.UcVFjCvl3PTPI8jiOnPbYA"
           {...this.state.viewport}
           onViewportChange={viewport => this.setState({ viewport })}
         />
+        {this._renderTooltip()}
       </div>
     );
   }
--- client/src/components/map/map-style.js
+++ client/src/components/map/map-style.js
@@ -3,3 +3,23 @@ import { fromJS } from "immutable";
 import MAP_STYLE from "../../constants/map-style.json";
 
 export const defaultMapStyle = fromJS(MAP_STYLE);
+
+export const gaugeLayer = fromJS({
+  id: "gauges",
+  source: "gauges",
+  type: "circle",
+  interactive: true,
+  minzoom: 5,
+  layout: {
+    visibility: "visible",
+  },
+  paint: {
+    "circle-radius": {
+      base: 3,
+      stops: [[5, 3.5], [7, 5.5]],
+    },
+    "circle-stroke-color": "#fafafa",
+    "circle-stroke-width": 1,
+    "circle-color": "#03a9f4",
+  },
+});
