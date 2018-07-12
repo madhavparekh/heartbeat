d12204c32536f4a27c5db18f0905a37123904cd6
Leo Q <leoq91@gmail.com>
2018-07-12 13:52:01 -0700

    de

--- client/src/components/home/DescriptionCard.js
+++ client/src/components/home/DescriptionCard.js
@@ -1,20 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
-//import classnames from "classnames";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
-//import Collapse from "@material-ui/core/Collapse";
-//import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import red from "@material-ui/core/colors/red";
-//import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import RaisedButton from "material-ui/RaisedButton";
-import Grid from "@material-ui/core/Grid";
 
-const styles = theme => ({
+const styles = () => ({
   card: {
     maxWidth: "100%",
   },
@@ -57,49 +52,30 @@ class DescriptionCard extends React.Component {
     const { classes } = this.props;
 
     return (
-      <div>
-        <Grid container>
-          <Grid item xs={12}>
-            <Card className={classes.card}>
-              <CardHeader title="Project Description" subheader="" />
-              <CardContent>
-                <Typography component="p" paragraph>
-                  When you go to the doctor and (s)he wants to know the strength
-                  of your heart, (s)he orders a cardiograph, which shows the
-                  your heartbeat as the seconds pass. Similarly, when we analyze
-                  rivers, we build “hydrographs” which in a very figurative
-                  analogy, hydrographs are the cardiographs of a river. A
-                  cardiograph show your heartbeat in five or ten seconds,
-                  however, river have a longer pace, their heartbeat cycle is
-                  not a few seconds but an entire year. Let’s start with a basin
-                  that I know pretty well, the Rio Grande basin (also known in
-                  Mexico as the Rio Bravo). We will call it the RGB for Rio
-                  Grande/Bravo. In its natural form, it had three main type of
-                  beats:
-                </Typography>
-              </CardContent>
-              <CardActions className={classes.actions} disableActionSpacing>
-                {/* <IconButton
-                  className={classnames(classes.expand, {
-                    [classes.expandOpen]: this.state.expanded,
-                  })}
-                  onClick={this.handleExpandClick}
-                  aria-expanded={this.state.expanded}
-                  aria-label="Show more"
-                >
-                  
-                  <ExpandMoreIcon />
-                </IconButton> */}
-                <RaisedButton
-                  label="Project Details"
-                  aria-label="Project Details"
-                  className={classes.projButton}
-                />
-              </CardActions>
-            </Card>
-          </Grid>
-        </Grid>
-      </div>
+      <Card className={classes.card}>
+        <CardHeader title="Project Description" subheader="" />
+        <CardContent>
+          <Typography component="p" paragraph>
+            When you go to the doctor and (s)he wants to know the strength of
+            your heart, (s)he orders a cardiograph, which shows the your
+            heartbeat as the seconds pass. Similarly, when we analyze rivers, we
+            build “hydrographs” which in a very figurative analogy, hydrographs
+            are the cardiographs of a river. A cardiograph show your heartbeat
+            in five or ten seconds, however, river have a longer pace, their
+            heartbeat cycle is not a few seconds but an entire year. Let’s start
+            with a basin that I know pretty well, the Rio Grande basin (also
+            known in Mexico as the Rio Bravo). We will call it the RGB for Rio
+            Grande/Bravo. In its natural form, it had three main type of beats:
+          </Typography>
+        </CardContent>
+        <CardActions className={classes.actions} disableActionSpacing>
+          <RaisedButton
+            label="Project Details"
+            aria-label="Project Details"
+            className={classes.projButton}
+          />
+        </CardActions>
+      </Card>
     );
   }
 }
--- client/src/components/login/LoginForm.js
+++ client/src/components/login/LoginForm.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import { Redirect } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { withStyles } from '@material-ui/core/styles';
-// import MenuItem from "@material-ui/core/MenuItem";
-import { TextField, FormHelperText, FormControl } from '@material-ui/core';
-import RaisedButton from 'material-ui/RaisedButton';
+import React from "react";
+import { Redirect } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { withStyles } from "@material-ui/core/styles";
+import { TextField, FormHelperText, FormControl } from "@material-ui/core";
+import RaisedButton from "material-ui/RaisedButton";
 
-import * as userAction from '../../actions/user';
+import * as userAction from "../../actions/user";
 
-const styles = (theme) => ({
+const styles = theme => ({
   container: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    display: "flex",
+    flexWrap: "wrap",
   },
   textField: {
     marginLeft: theme.spacing.unit,
@@ -28,8 +27,8 @@ class LoginForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
     };
   }
 
@@ -39,12 +38,6 @@ class LoginForm extends React.Component {
       [field]: e.target.value,
     });
   }
-
-  // handleChange = (email) => (event) => {
-  //   this.setState({
-  //     [email]: event.target.value,
-  //   });
-  // };
 
   render() {
     const { classes } = this.props;
@@ -59,7 +52,7 @@ class LoginForm extends React.Component {
               label="Email"
               className={classes.textField}
               placeholder="john@doe.com"
-              onChange={(e) => this.handleChange(e)}
+              onChange={e => this.handleChange(e)}
               margin="normal"
             />
           </FormControl>
@@ -70,13 +63,13 @@ class LoginForm extends React.Component {
               label="Password"
               className={classes.textField}
               placeholder="12345678"
-              onChange={(e) => this.handleChange(e)}
+              onChange={e => this.handleChange(e)}
               type="password"
               autoComplete="current-password"
               margin="normal"
             />
             {this.props.msg && (
-              <FormHelperText style={{ color: 'red' }}>
+              <FormHelperText style={{ color: "red" }}>
                 Invalid email and/or password
               </FormHelperText>
             )}
@@ -102,9 +95,10 @@ LoginForm.propTypes = {
   logInUser: PropTypes.func.isRequired,
   success: PropTypes.bool.isRequired,
   route: PropTypes.string.isRequired,
+  msg: PropTypes.string,
 };
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     success: state.user.success,
     msg: state.user.msg,
@@ -112,7 +106,7 @@ const mapStateToProps = (state) => {
   };
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
     logInUser: (email, password) =>
       dispatch(userAction.logInUser(email, password)),
